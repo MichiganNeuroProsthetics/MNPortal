f9837e85152b24587e1c693bb1bcfdb260cfba03
Brian Hsu <hsubrian@umich.edu>
2024-03-19 16:31:46 -0400

    remove svg css

--- components/card.jsx
+++ components/card.jsx
@@ -10,8 +10,13 @@ const Card = () => {
                     <p>Subteam</p>
                 </div>
                 <div className={styles.cardBottom}>
-                    <p>Projects?</p>
-                    <svg width="32" viewBox="0 -960 960 960" height="32" xmlns="http://www.w3.org/2000/svg"><path d="M226-160q-28 0-47-19t-19-47q0-28 19-47t47-19q28 0 47 19t19 47q0 28-19 47t-47 19Zm254 0q-28 0-47-19t-19-47q0-28 19-47t47-19q28 0 47 19t19 47q0 28-19 47t-47 19Zm254 0q-28 0-47-19t-19-47q0-28 19-47t47-19q28 0 47 19t19 47q0 28-19 47t-47 19ZM226-414q-28 0-47-19t-19-47q0-28 19-47t47-19q28 0 47 19t19 47q0 28-19 47t-47 19Zm254 0q-28 0-47-19t-19-47q0-28 19-47t47-19q28 0 47 19t19 47q0 28-19 47t-47 19Zm254 0q-28 0-47-19t-19-47q0-28 19-47t47-19q28 0 47 19t19 47q0 28-19 47t-47 19ZM226-668q-28 0-47-19t-19-47q0-28 19-47t47-19q28 0 47 19t19 47q0 28-19 47t-47 19Zm254 0q-28 0-47-19t-19-47q0-28 19-47t47-19q28 0 47 19t19 47q0 28-19 47t-47 19Zm254 0q-28 0-47-19t-19-47q0-28 19-47t47-19q28 0 47 19t19 47q0 28-19 47t-47 19Z"></path></svg>
+                    <p>Projects?</p>             
+                    <Image
+                        src="/burger-menu.svg" // Path to your SVG file in the public folder
+                        alt="Burger Menu Icon"
+                        width={24} // Adjust width and height as needed
+                        height={24}
+                    />
                 </div>
             </div>
             <div className={styles.cardImage}>
